37f6d290310f8749f263fc1193e9959f0888c799
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-01 13:50:40 +0300

    Added user input validation, email duplication check, password hashing, and user account storage in the database.

--- actions/auth.js
+++ actions/auth.js
@@ -1,5 +1,9 @@
 'use server';
 
+import { redirect } from "next/navigation.js";
+
+import { createUser } from "@/lib/user.js";
+
 export default async function register(prevState, formData) {
     const email = formData.get('email');
     const password = formData.get('password');
@@ -10,20 +14,29 @@ export default async function register(prevState, formData) {
         errors.email = 'Please enter a valid email address.'
     }
 
-    if(!password || password.length < 6){
+    if (!password || password.length < 6) {
         errors.password = 'Password must be at least 6 characters long.'
     }
 
-    if(Object.keys(errors).length > 0){
+    if (Object.keys(errors).length > 0) {
         return {
             errors
         }
     }
-    
-// storing in the DB
 
-    return {
-        ... prevStore,  
-        message: 'Registration successful!'
-    };
+    try {
+        createUser(email, password);
+    } catch (err) {
+        if (err.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+            return {
+                errors: {
+                    email: 'Account with this email already exist.'
+                }
+            }
+        }
+
+        throw err;
+    }
+
+    redirect('/training');
 }
